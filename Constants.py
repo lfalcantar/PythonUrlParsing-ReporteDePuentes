e5ef3754fee199d50a35166ba5b7c4bd9289a26f
lfalcantar <lfalcantar@miners.utep.edu>
2018-04-02 09:31:44 -0700

    add constants file for all tags names

---
+++ Constants.py
@@ -0,0 +1,11 @@
+URL = 'https://apps.cbp.gov/bwt/bwt.xml'  # define XML location live
+LOCAL_URL = ""
+#TODO find how to link to local xml
+
+PORT = "port"  #main attribute
+PORT_NUMBER = "port_number"
+
+
+#Time and date
+BORDER_WAIT_TIME = "border_wait_time"
+LAST_UPDATED_DATE = "last_updated_date"
